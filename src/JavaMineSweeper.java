ad140fa31a12cd926bef9ce2710610649f2d04f3
VGAliyev <v.aliev84@yandex.ru>
2023-11-18 20:08:41 +0400

    Images added and first used

--- src/JavaMineSweeper.java
+++ src/JavaMineSweeper.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class JavaMineSweeper extends JFrame {
     private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
 
@@ -15,8 +19,14 @@ public class JavaMineSweeper extends JFrame {
     }
 
     private void initPanel() {
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500, 500));
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.drawImage(getImage("bomb"), 0, 0, this);
+            }
+        };
+        panel.setPreferredSize(new Dimension(
+                COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
         add(panel);
     }
 
@@ -27,5 +37,11 @@ public class JavaMineSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+    }
+
+    private Image getImage(String name) {
+        String fileName = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource(fileName)));
+        return icon.getImage();
     }
 }
